73472ef86fa0f25bce89a778cc30f34ab58bc25d
Axel3098 <axel.navarrotlavigne@viacesi.fr>
2016-12-12 10:42:43 +0100

    Modif programme Launcher (random)

--- ChoixPBMRandom.c
+++ ChoixPBMRandom.c
@@ -11,39 +11,28 @@ int main(void)
 
     srand(time(NULL));      //initialisation de rand
     nombre_aleatoire = rand();    //nombre_aleatoire prend la valeur d'un nombre aleatoire grace a la fonction rand()
-    nombre = nombre_aleatoire %5;    //on souhaite un nombre aleatoire de 5 valeurs possible, on utilise donc modulo 5
+    nombre = nombre_aleatoire %3;    //on souhaite un nombre aleatoire de 5 valeurs possible, on utilise donc modulo 5
     printf("%d\n", nombre);        //on verifie la valeur du nombre aleatoire avec un printf
 
-    char *programme[] = {"bonjour.c","salut.c","yo.c","plop.c","ouech.c",NULL};    //on stock le nom des programmes dans un tableau char
+    char *programme[] = {"statique.c","dynamique.c","interactif.c", NULL};//on stock le nom des programmes dans un tableau char
 
+    system("clear"); //Vide la console avant de lancer les autres programmes
     if (nombre == 0)
 
     {                        //selon la valeur du nombre aleatoire
-        execv("./bonjour", programme);        //on execute la commande permettant de lancer un programme
+        execv("./statique", programme);        //on execute la commande permettant de lancer un programme
     }                        //le nom de ce programme est stocke dans le tableau char
 
     else if (nombre == 1)
 
     {
-        execv("./salut", programme);
+        execv("./dynamique", programme);
     }
 
     else if (nombre == 2)
 
     {
-        execv("./yo", programme);
-    }
-
-       else if (nombre == 3)
-
-       {
-               execv("./plop", programme);
-       }
-
-    else
-
-    {
-        execv("./ouech", programme);
+        execv("./interactif", programme);
     }
 
 }
