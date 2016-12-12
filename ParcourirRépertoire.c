4b06e531b168edaa30dee4806a65c8e731f36e80
Axel3098 <axel.navarrotlavigne@viacesi.fr>
2016-12-12 10:25:05 +0100

    Nouveaux programmes en test

---
+++ ParcourirRépertoire.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+    DIR* rep = opendir("EXIA_SAVER1_PBM");
+
+    if(rep != NULL)
+    {
+        struct dirent *ent;
+
+        while ((ent = readdir (rep)) != NULL)
+        {
+            printf("%s\n", ent->d_name);
+        }
+        closedir(rep);
+    }
+    return 0;
+}
