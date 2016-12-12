66cd56cbe27444b17877a976db1fc043f2d9e9cb
Axel3098 <axel.navarrotlavigne@viacesi.fr>
2016-12-12 15:22:01 +0100

    Script pour les variables d'environnement

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export EXIASAVER_HOME="$HOME/Documents/Project-C/"
+export EXIASAVER1_PBM="$HOME/Documents/Project-C/Statique_PBM/"
+export EXIASAVER2_PBM="$HOME/Documents/Project-C/Dynamique_PBM/"
+export EXIASAVER2_TAILLE="5x3"
+export EXIASAVER2_SLEEP="5"
+export EXIASAVER3_PBM="$HOME/Documents/Project-C/Interactif_PBM/"
