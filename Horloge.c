2ddb9b0d40a34dab8943a7a16cc198d7dfba9a99
Axel3098 <axel.navarrotlavigne@viacesi.fr>
2016-12-13 09:54:52 +0100

    Modif des programmesavec les variables d'environnement

--- Horloge.c
+++ Horloge.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #define FRANCE +1
-
+#include <string.h>
+#include <unistd.h>
 
 void afficherHeureHorloge()
 {
@@ -11,18 +12,30 @@ void afficherHeureHorloge()
 
     time(&temps);
     date = *gmtime(&temps);
-    printf(" %d:%d:%d\n",(date.tm_hour+FRANCE+24)%24, date.tm_min, date.tm_sec);
+    printf("%d:%d:%d\n",(date.tm_hour+FRANCE+24)%24, date.tm_min, date.tm_sec);
 }
 
 void afficherPhraseAActualiser()
 {
-    int j =0;
+    int j = 0;
+    char* str = getenv("EXIASAVER2_SLEEP");
 
+    //str = NULL; //pour tester si le else fonctionne correctement
+
+    char dodo[2];
+
+    if(str != NULL)
+        strcpy(dodo, str);
+
+    else
+    strcpy(dodo, "10");
+
+    int n= atoi(dodo);
     printf("L'heure va etre actualisée dans quelques secondes");
 
     fflush(stdout);
 
-    while (j<5)
+    while (j<n)
     {
         printf(".");
         fflush(stdout);
